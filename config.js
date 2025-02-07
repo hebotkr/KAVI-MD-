a4eee434f384274bdea21bf9359f63d73091c5e2
kaviduRs <githubisban@gmail.com>
2025-02-07 14:27:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,5 @@ function convertToBool(text, fault = "true") {
 }
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "Q0IQ2Z7S#T7hi_MrPRnTbSftp1vqblPVTER9mCrN-if5HY751Qew",
-  MONGODB: process.env.MONGODB || "mongodb://mongo:TsujJhsGdxTaOgvIYZgbnYQIoxltXamn@roundhouse.proxy.rlwy.net:48091",
   OWNER_NUM: process.env.OWNER_NUM || "94718108364",
 };
