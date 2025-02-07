5c7ae2286b3bda2b2f18d702f896561eb2b8356b
kaviduRs <githubisban@gmail.com>
2025-02-07 20:26:36 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -3,7 +3,7 @@ const config = require('../config');
 cmd({
     pattern: "alive",
     alias: ["bot"],
-    react: "🍀",
+    react: "🎗️",
     desc: "Check bot online or no.",
     category: "main",
     filename: __filename
