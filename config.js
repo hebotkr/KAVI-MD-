157824b125a5233d52c156d3856aa06e7b37d3dc
kaviduRs <githubisban@gmail.com>
2025-02-08 10:30:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "Q0IQ2Z7S#T7hi_MrPRnTbSftp1vqblPVTER9mCrN-if5HY751Qew",
   OWNER_NUM: process.env.OWNER_NUM || "94718108364",
   PREFIX: process.env.PREFIX || ".",
-  ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/kaviduRs/MD-HELP/raw/refs/heads/main/img/b1e19851-f5ec-49fe-8d32-d45fbf654021.webp",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/kaviduRs/MD-HELP/refs/heads/main/img/IMG-20250205-WA0002.jpg",
   ALIVE_MSG: process.env.ALIVE_MSG || "*yes I'm Alive noW!* 🙈\n\n> 𝐌𝐚𝐝𝐞 𝐛𝐲 *𝐊𝐀𝐕𝐈𝐃𝐔 𝐑𝐀𝐒𝐀𝐍𝐆𝐀*  🎗️",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
   MODE: process.env.MODE || "public",
